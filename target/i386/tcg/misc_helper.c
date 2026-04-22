373c2d78ef98cef9ff470e96a12e11644e635e57
pihaoxuan <haoxuanpi@163.com>
2026-04-22 14:23:55 +0800

    注释之前的helper函数，并增加新的helper函数，新函数开启硬件拓展并分寄存器操作和内存操作两个函数

--- target/i386/tcg/misc_helper.c
+++ target/i386/tcg/misc_helper.c
@@ -150,112 +150,306 @@ target_ulong HELPER(rdpid)(CPUX86State *env)
 #include "accel/tcg/cpu-ldst.h"
 #include <math.h>
 
-void helper_custom_fast_fma(CPUX86State *env, uint32_t dest, uint32_t src1, uint32_t src2, target_ulong vaddr, uint32_t flags) {
-    // 获取宿主机的指令返回地址，极速访问内存发生缺页时，能精准恢复虚拟机上下文
-    uintptr_t ra = GETPC(); 
-    
-    // 解析标志位
-    uint32_t oprsz = flags & 0xFFFF;
-    uint32_t opcode = (flags >> 16) & 0xFF;
-    uint32_t w_bit = (flags >> 24) & 1;
-    
-    int element_count = oprsz / ((w_bit == 1) ? 8 : 4);
+//void helper_custom_fast_fma(CPUX86State *env, uint32_t dest, uint32_t src1, uint32_t src2, target_ulong vaddr, uint32_t flags) {
+//    // 获取宿主机的指令返回地址，极速访问内存发生缺页时，能精准恢复虚拟机上下文
+//    uintptr_t ra = GETPC(); 
+//    
+//    // 解析标志位
+//    uint32_t oprsz = flags & 0xFFFF;
+//    uint32_t opcode = (flags >> 16) & 0xFF;
+//    uint32_t w_bit = (flags >> 24) & 1;
+//    
+//    int element_count = oprsz / ((w_bit == 1) ? 8 : 4);
+//
+//    if (w_bit == 1) { // 64位 double (pd)
+//        double * __restrict dest_ptr = (double *)&env->xmm_regs[dest];
+//        double * __restrict src1_ptr = (double *)&env->xmm_regs[src1];
+//        double * __restrict src2_ptr;
+//        double mem_src2_pd[4] = {0};
+//
+//        if (src2 != 0xFFFFFFFF) {
+//            src2_ptr = (double *)&env->xmm_regs[src2];
+//        } else {
+//            for (int i = 0; i < element_count; i++) {
+//                union { uint64_t i; double d; } conv;
+//                conv.i = cpu_ldq_data_ra(env, vaddr + i * 8, ra);
+//                mem_src2_pd[i] = conv.d;
+//            }
+//            src2_ptr = mem_src2_pd;
+//        }
+//
+//        for (int i = 0; i < element_count; i++) {
+//            if (opcode == 0xB8) dest_ptr[i] = (src1_ptr[i] * src2_ptr[i]) + dest_ptr[i];
+//            else if (opcode == 0xA8) dest_ptr[i] = (dest_ptr[i] * src1_ptr[i]) + src2_ptr[i];
+//            else if (opcode == 0x98) dest_ptr[i] = (dest_ptr[i] * src2_ptr[i]) + src1_ptr[i];
+//        }
+//    } else { // 32位 float (ps)
+//        float * __restrict dest_ptr = (float *)&env->xmm_regs[dest];
+//        float * __restrict src1_ptr = (float *)&env->xmm_regs[src1];
+//        float * __restrict src2_ptr;
+//        float mem_src2_ps[8] = {0};
+//
+//        if (src2 != 0xFFFFFFFF) {
+//            src2_ptr = (float *)&env->xmm_regs[src2];
+//        } else {
+//            for (int i = 0; i < element_count; i++) {
+//                union { uint32_t i; float f; } conv;
+//                conv.i = cpu_ldl_data_ra(env, vaddr + i * 4, ra);
+//                mem_src2_ps[i] = conv.f;
+//            }
+//            src2_ptr = mem_src2_ps;
+//        }
+//	
+//#pragma GCC ivdep
+//        for (int i = 0; i < element_count; i++) {
+//            if (opcode == 0xB8) dest_ptr[i] = (src1_ptr[i] * src2_ptr[i]) + dest_ptr[i];
+//            else if (opcode == 0xA8) dest_ptr[i] = (dest_ptr[i] * src1_ptr[i]) + src2_ptr[i];
+//            else if (opcode == 0x98) dest_ptr[i] = (dest_ptr[i] * src2_ptr[i]) + src1_ptr[i];
+//        }
+//    }
+//
+//    // 清除 YMM 高 128 位
+//    if (oprsz == 16) {
+//        env->xmm_regs[dest].ZMM_Q(2) = 0;
+//        env->xmm_regs[dest].ZMM_Q(3) = 0;
+//    }
+//}
+//
+//
+//
+//
+//
+//uint64_t helper_custom_fma_chunk(uint64_t d, uint64_t a, uint64_t b, uint32_t flags) {
+//    uint32_t opcode = flags & 0xFF;
+//    uint32_t w_bit = (flags >> 8) & 1;
+//    uint32_t chunk_idx = (flags >> 16) & 0xFF;
+//
+//    bool is_scalar = (opcode == 0x99 || opcode == 0xA9 || opcode == 0xB9);
+//
+//    if (w_bit == 1) { 
+//        // 64位数据包含 1 个 double
+//        union { uint64_t i; double f; } vd, va, vb, vres;
+//        vd.i = d; va.i = a; vb.i = b;
+//        
+//	if(is_scalar && chunk_idx > 0){
+//	    vres.f = vd.f;
+//	}else{
+//            if (opcode == 0xB8) vres.f = fma(va.f, vb.f, vd.f);
+//            else if (opcode == 0xA8) vres.f = fma(vd.f, va.f, vb.f);
+//            else if (opcode == 0x98) vres.f = fma(vd.f, vb.f, va.f);
+//            else vres.f = vd.f;
+//	}
+//        return vres.i;
+//    } else { 
+//        // 64位数据包含 2 个 float
+//        union { uint64_t i; float f[2]; } vd, va, vb, vres;
+//        vd.i = d; va.i = a; vb.i = b;
+//        
+//        for (int j = 0; j < 2; j++) {
+//	    if(is_scalar && (chunk_idx > 0 || j > 0)){
+//		vres.f[j] = vd.f[j];
+//	    }else{
+//                if (opcode == 0xB8) vres.f[j] = fmaf(va.f[j], vb.f[j], vd.f[j]);
+//                else if (opcode == 0xA8) vres.f[j] = fmaf(vd.f[j], va.f[j], vb.f[j]);
+//                else if (opcode == 0x98) vres.f[j] = fmaf(vd.f[j], vb.f[j], va.f[j]);
+//                else vres.f[j] = vd.f[j];
+//	    }
+//        }
+//        
+//        return vres.i;
+//    }
+//}
 
-    if (w_bit == 1) { // 64位 double (pd)
-        double * __restrict dest_ptr = (double *)&env->xmm_regs[dest];
-        double * __restrict src1_ptr = (double *)&env->xmm_regs[src1];
-        double * __restrict src2_ptr;
-        double mem_src2_pd[4] = {0};
 
-        if (src2 != 0xFFFFFFFF) {
-            src2_ptr = (double *)&env->xmm_regs[src2];
-        } else {
-            for (int i = 0; i < element_count; i++) {
-                union { uint64_t i; double d; } conv;
-                conv.i = cpu_ldq_data_ra(env, vaddr + i * 8, ra);
-                mem_src2_pd[i] = conv.d;
+#include <lasxintrin.h> // 龙芯 256-bit 向量指令集 (LASX)
+#include <lsxintrin.h>  // 龙芯 128-bit 向量指令集 (LSX)
+// #include "exec/cpu_ldst.h" // 需要用到内存访问接口和 GETPC()
+
+// ====================================================================
+// 处理纯寄存器操作数的 Helper
+// ====================================================================
+void helper_custom_fma_vector_reg(CPUX86State *env, uint32_t dofs, uint32_t aofs, uint32_t bofs, uint32_t flags) {
+    uint32_t opcode = flags & 0xFF;
+    uint32_t w_bit = (flags >> 8) & 1;
+    uint32_t l_bit = (flags >> 9) & 1;
+    bool is_scalar = (opcode == 0x99 || opcode == 0xA9 || opcode == 0xB9);
+
+    void *pd = (void *)env + dofs;
+    void *pa = (void *)env + aofs;
+    void *pb = (void *)env + bofs;
+
+    if (l_bit == 1) { // 256-bit YMM (LASX)
+        if (w_bit == 1) { // Double (4个双精度)
+            __m256d vd = (__m256d)__lasx_xvld(pd, 0);
+            __m256d va = (__m256d)__lasx_xvld(pa, 0);
+            __m256d vb = (__m256d)__lasx_xvld(pb, 0);
+            __m256d vres;
+
+            if (opcode == 0xB8) vres = __lasx_xvfmadd_d(va, vb, vd);      // a*b+d
+            else if (opcode == 0xA8) vres = __lasx_xvfmadd_d(vd, va, vb); // d*a+b
+            else if (opcode == 0x98) vres = __lasx_xvfmadd_d(vd, vb, va); // d*b+a
+            else vres = vd;
+            
+            // 兼容你的标量保留逻辑
+            if (is_scalar) {
+                double *res_f = (double *)&vres;
+                double *d_f = (double *)pd;
+                for (int j = 1; j < 4; j++) res_f[j] = d_f[j];
             }
-            src2_ptr = mem_src2_pd;
-        }
+            __lasx_xvst(vres, pd, 0);
+            
+        } else { // Float (8个单精度)
+            __m256 vd = (__m256)__lasx_xvld(pd, 0);
+            __m256 va = (__m256)__lasx_xvld(pa, 0);
+            __m256 vb = (__m256)__lasx_xvld(pb, 0);
+            __m256 vres;
 
-        for (int i = 0; i < element_count; i++) {
-            if (opcode == 0xB8) dest_ptr[i] = (src1_ptr[i] * src2_ptr[i]) + dest_ptr[i];
-            else if (opcode == 0xA8) dest_ptr[i] = (dest_ptr[i] * src1_ptr[i]) + src2_ptr[i];
-            else if (opcode == 0x98) dest_ptr[i] = (dest_ptr[i] * src2_ptr[i]) + src1_ptr[i];
-        }
-    } else { // 32位 float (ps)
-        float * __restrict dest_ptr = (float *)&env->xmm_regs[dest];
-        float * __restrict src1_ptr = (float *)&env->xmm_regs[src1];
-        float * __restrict src2_ptr;
-        float mem_src2_ps[8] = {0};
+            if (opcode == 0xB8) vres = __lasx_xvfmadd_s(va, vb, vd);
+            else if (opcode == 0xA8) vres = __lasx_xvfmadd_s(vd, va, vb);
+            else if (opcode == 0x98) vres = __lasx_xvfmadd_s(vd, vb, va);
+            else vres = vd;
 
-        if (src2 != 0xFFFFFFFF) {
-            src2_ptr = (float *)&env->xmm_regs[src2];
-        } else {
-            for (int i = 0; i < element_count; i++) {
-                union { uint32_t i; float f; } conv;
-                conv.i = cpu_ldl_data_ra(env, vaddr + i * 4, ra);
-                mem_src2_ps[i] = conv.f;
+            if (is_scalar) {
+                float *res_f = (float *)&vres;
+                float *d_f = (float *)pd;
+                for (int j = 1; j < 8; j++) res_f[j] = d_f[j];
             }
-            src2_ptr = mem_src2_ps;
+            __lasx_xvst(vres, pd, 0);
         }
-	
-#pragma GCC ivdep
-        for (int i = 0; i < element_count; i++) {
-            if (opcode == 0xB8) dest_ptr[i] = (src1_ptr[i] * src2_ptr[i]) + dest_ptr[i];
-            else if (opcode == 0xA8) dest_ptr[i] = (dest_ptr[i] * src1_ptr[i]) + src2_ptr[i];
-            else if (opcode == 0x98) dest_ptr[i] = (dest_ptr[i] * src2_ptr[i]) + src1_ptr[i];
-        }
-    }
+    } else { // 128-bit XMM (LSX)
+        if (w_bit == 1) { // Double (2个双精度)
+            __m128d vd = (__m128d)__lsx_vld(pd, 0);
+            __m128d va = (__m128d)__lsx_vld(pa, 0);
+            __m128d vb = (__m128d)__lsx_vld(pb, 0);
+            __m128d vres;
 
-    // 清除 YMM 高 128 位
-    if (oprsz == 16) {
-        env->xmm_regs[dest].ZMM_Q(2) = 0;
-        env->xmm_regs[dest].ZMM_Q(3) = 0;
+            if (opcode == 0xB8) vres = __lsx_vfmadd_d(va, vb, vd);
+            else if (opcode == 0xA8) vres = __lsx_vfmadd_d(vd, va, vb);
+            else if (opcode == 0x98) vres = __lsx_vfmadd_d(vd, vb, va);
+            else vres = vd;
+
+            if (is_scalar) {
+                double *res_f = (double *)&vres;
+                double *d_f = (double *)pd;
+                res_f[1] = d_f[1];
+            }
+            __lsx_vst(vres, pd, 0);
+        } else { // Float (4个单精度)
+            __m128 vd = (__m128)__lsx_vld(pd, 0);
+            __m128 va = (__m128)__lsx_vld(pa, 0);
+            __m128 vb = (__m128)__lsx_vld(pb, 0);
+            __m128 vres;
+
+            if (opcode == 0xB8) vres = __lsx_vfmadd_s(va, vb, vd);
+            else if (opcode == 0xA8) vres = __lsx_vfmadd_s(vd, va, vb);
+            else if (opcode == 0x98) vres = __lsx_vfmadd_s(vd, vb, va);
+            else vres = vd;
+
+            if (is_scalar) {
+                float *res_f = (float *)&vres;
+                float *d_f = (float *)pd;
+                for (int j = 1; j < 4; j++) res_f[j] = d_f[j];
+            }
+            __lsx_vst(vres, pd, 0);
+        }
     }
 }
 
-
-
-
-
-uint64_t helper_custom_fma_chunk(uint64_t d, uint64_t a, uint64_t b, uint32_t flags) {
+// ====================================================================
+// 处理带内存操作数的 Helper
+// ====================================================================
+void helper_custom_fma_vector_mem(CPUX86State *env, uint32_t dofs, uint32_t aofs, target_ulong vaddr, uint32_t flags) {
+    uintptr_t ra = GETPC(); // 捕获异常返回地址，用于引发缺页异常时回溯
     uint32_t opcode = flags & 0xFF;
     uint32_t w_bit = (flags >> 8) & 1;
-    uint32_t chunk_idx = (flags >> 16) & 0xFF;
-
+    uint32_t l_bit = (flags >> 9) & 1;
     bool is_scalar = (opcode == 0x99 || opcode == 0xA9 || opcode == 0xB9);
 
-    if (w_bit == 1) { 
-        // 64位数据包含 1 个 double
-        union { uint64_t i; double f; } vd, va, vb, vres;
-        vd.i = d; va.i = a; vb.i = b;
-        
-	if(is_scalar && chunk_idx > 0){
-	    vres.f = vd.f;
-	}else{
-            if (opcode == 0xB8) vres.f = fma(va.f, vb.f, vd.f);
-            else if (opcode == 0xA8) vres.f = fma(vd.f, va.f, vb.f);
-            else if (opcode == 0x98) vres.f = fma(vd.f, vb.f, va.f);
-            else vres.f = vd.f;
-	}
-        return vres.i;
-    } else { 
-        // 64位数据包含 2 个 float
-        union { uint64_t i; float f[2]; } vd, va, vb, vres;
-        vd.i = d; va.i = a; vb.i = b;
-        
-        for (int j = 0; j < 2; j++) {
-	    if(is_scalar && (chunk_idx > 0 || j > 0)){
-		vres.f[j] = vd.f[j];
-	    }else{
-                if (opcode == 0xB8) vres.f[j] = fmaf(va.f[j], vb.f[j], vd.f[j]);
-                else if (opcode == 0xA8) vres.f[j] = fmaf(vd.f[j], va.f[j], vb.f[j]);
-                else if (opcode == 0x98) vres.f[j] = fmaf(vd.f[j], vb.f[j], va.f[j]);
-                else vres.f[j] = vd.f[j];
-	    }
+    // 核心安全点：使用 QEMU API 读取 Guest 内存并存放到宿主机的栈对齐数组中
+    __attribute__((aligned(32))) uint64_t mem_buf[4] = {0};
+    mem_buf[0] = cpu_ldq_data_ra(env, vaddr, ra);
+    mem_buf[1] = cpu_ldq_data_ra(env, vaddr + 8, ra);
+    if (l_bit == 1) {
+        mem_buf[2] = cpu_ldq_data_ra(env, vaddr + 16, ra);
+        mem_buf[3] = cpu_ldq_data_ra(env, vaddr + 24, ra);
+    }
+
+    void *pd = (void *)env + dofs;
+    void *pa = (void *)env + aofs;
+    void *pb = (void *)mem_buf; // 将读入的内存作为操作数 b
+
+    // -------- 接下来的计算逻辑与 Register Helper 完全一致 --------
+    if (l_bit == 1) { // 256-bit YMM (LASX)
+        if (w_bit == 1) {
+            __m256d vd = (__m256d)__lasx_xvld(pd, 0);
+            __m256d va = (__m256d)__lasx_xvld(pa, 0);
+            __m256d vb = (__m256d)__lasx_xvld(pb, 0);
+            __m256d vres;
+
+            if (opcode == 0xB8) vres = __lasx_xvfmadd_d(va, vb, vd);
+            else if (opcode == 0xA8) vres = __lasx_xvfmadd_d(vd, va, vb);
+            else if (opcode == 0x98) vres = __lasx_xvfmadd_d(vd, vb, va);
+            else vres = vd;
+            
+            if (is_scalar) {
+                double *res_f = (double *)&vres;
+                double *d_f = (double *)pd;
+                for (int j = 1; j < 4; j++) res_f[j] = d_f[j];
+            }
+            __lasx_xvst(vres, pd, 0);
+        } else {
+            __m256 vd = (__m256)__lasx_xvld(pd, 0);
+            __m256 va = (__m256)__lasx_xvld(pa, 0);
+            __m256 vb = (__m256)__lasx_xvld(pb, 0);
+            __m256 vres;
+
+            if (opcode == 0xB8) vres = __lasx_xvfmadd_s(va, vb, vd);
+            else if (opcode == 0xA8) vres = __lasx_xvfmadd_s(vd, va, vb);
+            else if (opcode == 0x98) vres = __lasx_xvfmadd_s(vd, vb, va);
+            else vres = vd;
+
+            if (is_scalar) {
+                float *res_f = (float *)&vres;
+                float *d_f = (float *)pd;
+                for (int j = 1; j < 8; j++) res_f[j] = d_f[j];
+            }
+            __lasx_xvst(vres, pd, 0);
         }
-        
-        return vres.i;
+    } else { // 128-bit XMM (LSX)
+        if (w_bit == 1) {
+            __m128d vd = (__m128d)__lsx_vld(pd, 0);
+            __m128d va = (__m128d)__lsx_vld(pa, 0);
+            __m128d vb = (__m128d)__lsx_vld(pb, 0);
+            __m128d vres;
+
+            if (opcode == 0xB8) vres = __lsx_vfmadd_d(va, vb, vd);
+            else if (opcode == 0xA8) vres = __lsx_vfmadd_d(vd, va, vb);
+            else if (opcode == 0x98) vres = __lsx_vfmadd_d(vd, vb, va);
+            else vres = vd;
+
+            if (is_scalar) {
+                double *res_f = (double *)&vres;
+                double *d_f = (double *)pd;
+                res_f[1] = d_f[1];
+            }
+            __lsx_vst(vres, pd, 0);
+        } else {
+            __m128 vd = (__m128)__lsx_vld(pd, 0);
+            __m128 va = (__m128)__lsx_vld(pa, 0);
+            __m128 vb = (__m128)__lsx_vld(pb, 0);
+            __m128 vres;
+
+            if (opcode == 0xB8) vres = __lsx_vfmadd_s(va, vb, vd);
+            else if (opcode == 0xA8) vres = __lsx_vfmadd_s(vd, va, vb);
+            else if (opcode == 0x98) vres = __lsx_vfmadd_s(vd, vb, va);
+            else vres = vd;
+
+            if (is_scalar) {
+                float *res_f = (float *)&vres;
+                float *d_f = (float *)pd;
+                for (int j = 1; j < 4; j++) res_f[j] = d_f[j];
+            }
+            __lsx_vst(vres, pd, 0);
+        }
     }
 }
